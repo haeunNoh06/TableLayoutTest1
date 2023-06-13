eb6a185490c890346b60e451821cad26748a9277
haeunNoh06 <okhagrace06@naver.com>
2023-06-13 19:52:56 +0900

    TableLayout 사용한 계산기(키패드 있음)

--- app/src/main/java/kr/hs/emirim/soeun/tablelayouttest1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/soeun/tablelayouttest1/MainActivity.kt
@@ -2,9 +2,11 @@ package kr.hs.emirim.soeun.tablelayouttest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,11 +28,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.calculator)
 
         edit1 = findViewById(R.id.edit1)
-        edit1 = findViewById(R.id.edit2)
+        edit2 = findViewById(R.id.edit2)
         btnPlus = findViewById(R.id.btnPlus)
         btnMinus = findViewById(R.id.btnMinus)
         btnMultiply = findViewById(R.id.btnMultiply)
         btnDivide = findViewById(R.id.btnDivide)
         textResult = findViewById(R.id.textResult)
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+
+        for ( i in 0..9 step 1) {
+            btns.add(findViewById(btnIds[i]))
+        }
+        for ( i in 0 .. btnIds.size-1 step 1) {
+            btns[i].setOnClickListener {
+                if ( edit1.isFocused ) {
+                    num1 = edit1.text.toString() + btns[i].text.toString()
+                    edit1.setText(num1)
+                } else if ( edit2.isFocused ) {
+                    num2 = edit2.text.toString() + btns[i].text.toString()
+                    edit2.setText(num2)
+                } else {
+                    Toast.makeText(applicationContext, "먼저 입력할 곳에 포커스를 주세요~", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }
+
     }
+        var btnListener = OnClickListener{
+            num1 = edit1.text.toString()
+            num2 = edit2.text.toString()
+
+            when(it.id) {
+                R.id.btnPlus -> result = num1.toInt() + num2.toInt()
+                R.id.btnMinus -> result = num1.toInt() - num2.toInt()
+                R.id.btnMultiply -> result = num1.toInt() * num2.toInt()
+                R.id.btnDivide -> {
+                    if ( num1 == "" || num2 == "") {
+                        Toast.makeText(baseContext, "숫자를 입력하지 않고 버튼을 누르면 안됨!!",Toast.LENGTH_LONG)
+                        return@OnClickListener
+                    }
+                    result = num1.toInt() / num2.toInt()
+                }
+            }
+            textResult.setText(R.string.text_result)
+            textResult.append(result.toString())
+        }
 }
